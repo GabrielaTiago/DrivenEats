aa1f82e50b577e1bf1c2c1e99d72931d8d35660e
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-12-17 19:30:27 -0300

    refatora as funções de selecionar itens do pedido

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,61 +1,83 @@
-function selecionarPrato(prato){
-    let pratoSelecionado = document.querySelector(".itens-rolagem-pratos .escolhido");
+function selecionarPrato(prato) {
+  let pratoSelecionado = document.querySelector(".pratos .escolhido");
 
-    if(pratoSelecionado !== null){
-        pratoSelecionado.classList.remove("escolhido");
-    }
-    prato.classList.add("escolhido");
-    verificarSelecao()
+  if (!!pratoSelecionado) {
+    pratoSelecionado.classList.remove("escolhido");
+  }
+
+  prato.classList.add("escolhido");
+  verificarSelecao();
 }
 
-function selecionarBebidas(bebida){
-    let bebidaSelecionada = document.querySelector(".itens-rolagem-bebidas .escolhido");
+function selecionarBebidas(bebida) {
+  let bebidaSelecionada = document.querySelector(".bebidas .escolhido");
 
-    if(bebidaSelecionada !== null){
-        bebidaSelecionada.classList.remove("escolhido");
-    }
-    bebida.classList.add("escolhido");
-    verificarSelecao()
+  if (!!bebidaSelecionada) {
+    bebidaSelecionada.classList.remove("escolhido");
+  }
+
+  bebida.classList.add("escolhido");
+  verificarSelecao();
 }
 
-function selecionarSobremesas(sobremesa){
-    let sobremesaSelecionada = document.querySelector(".itens-rolagem-sobremesas .escolhido");
+function selecionarSobremesas(sobremesa) {
+  let sobremesaSelecionada = document.querySelector(".sobremesas .escolhido");
 
-    if(sobremesaSelecionada !== null){
-        sobremesaSelecionada.classList.remove("escolhido");
-    }
-    sobremesa.classList.add("escolhido");
-    verificarSelecao()
+  if (!!sobremesaSelecionada) {
+    sobremesaSelecionada.classList.remove("escolhido");
+  }
+
+  sobremesa.classList.add("escolhido");
+  verificarSelecao();
 }
 
-function verificarSelecao(){
-    const verifica = document.querySelectorAll(".escolhido").length;
-    const footer = document.querySelector('footer')
+function verificarSelecao() {
+  const verifica = document.querySelectorAll(".escolhido").length;
+  const footer = document.querySelector("footer");
 
-    if(verifica === 3) {
-    footer.firstElementChild.classList.add('escondido')
-    footer.lastElementChild.classList.remove('escondido')
-    }
+  if (verifica === 3) {
+    footer.firstElementChild.classList.add("escondido");
+    footer.lastElementChild.classList.remove("escondido");
+  }
 }
 
-function fechandoPedido(){
-    let localPratoSelecionado = document.querySelector(".itens-rolagem-pratos .escolhido");
-    let localBebidaSelecionada = document.querySelector(".itens-rolagem-bebidas .escolhido");
-    let localSobremesaSelecionada = document.querySelector(".itens-rolagem-sobremesas .escolhido");
+function fechandoPedido() {
+  let pratoSelecionado = document.querySelector(".pratos .escolhido");
+  let bebidaSelecionada = document.querySelector(".bebidas .escolhido");
+  let sobremesaSelecionada = document.querySelector(".sobremesas .escolhido");
 
-    let nomePratoSelecionado = localPratoSelecionado.querySelector(".nome-item").innerHTML;
-    let nomeBebidaSelecionada = localBebidaSelecionada.querySelector(".nome-item").innerHTML;
-    let nomeSobremesaSelecionada = localSobremesaSelecionada.querySelector(".nome-item").innerHTML;
+  let nomePratoSelecionado =
+    pratoSelecionado.querySelector(".nome-item").innerHTML;
+  let nomeBebidaSelecionada =
+    bebidaSelecionada.querySelector(".nome-item").innerHTML;
+  let nomeSobremesaSelecionada =
+    sobremesaSelecionada.querySelector(".nome-item").innerHTML;
 
-    let precoPratoSelecionado = localPratoSelecionado.querySelector(".valor").innerHTML.replace("R$","").replace(",",".");
-    let precoBebidaSelecionada = localBebidaSelecionada.querySelector(".valor").innerHTML.replace("R$","").replace(",",".");
-    let precoSobremesaSelecionada = localSobremesaSelecionada.querySelector(".valor").innerHTML.replace("R$","").replace(",",".");
+  let precoPratoSelecionado = pratoSelecionado
+    .querySelector(".valor")
+    .innerHTML.replace("R$", "")
+    .replace(",", ".");
+  let precoBebidaSelecionada = bebidaSelecionada
+    .querySelector(".valor")
+    .innerHTML.replace("R$", "")
+    .replace(",", ".");
+  let precoSobremesaSelecionada = sobremesaSelecionada
+    .querySelector(".valor")
+    .innerHTML.replace("R$", "")
+    .replace(",", ".");
 
-    let precoTotal = (Number(precoPratoSelecionado) + Number(precoBebidaSelecionada) + Number(precoSobremesaSelecionada)).toFixed(2);
+  let precoTotal = (
+    Number(precoPratoSelecionado) +
+    Number(precoBebidaSelecionada) +
+    Number(precoSobremesaSelecionada)
+  ).toFixed(2);
 
-    window.open("https://wa.me/5562991769161?text="+encodeURIComponent(`Olá, gostaria de fazer o pedido:
-- Prato: ${nomePratoSelecionado}
-- Bebida: ${nomeBebidaSelecionada}
-- Sobremesa: ${nomeSobremesaSelecionada}
-Total: R$ ${precoTotal}`));
+  window.open(
+    "https://wa.me/5562991769161?text=" +
+      encodeURIComponent(`Olá, gostaria de fazer o pedido:
+        - Prato: ${nomePratoSelecionado}
+        - Bebida: ${nomeBebidaSelecionada}
+        - Sobremesa: ${nomeSobremesaSelecionada}
+        Total: R$ ${precoTotal}`)
+  );
 }
